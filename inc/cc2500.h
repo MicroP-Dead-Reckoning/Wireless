bf0b995c6a4503bf80b6d15cc2bd46bc0fa7cca7
Jit <kanetkarster@gmail.com>
2015-11-11 16:58:30 -0500

    started working on init structure for wireless

--- inc/cc2500.h
+++ inc/cc2500.h
@@ -1,4 +1,84 @@
+/**
+  ******************************************************************************
+  * @file    cc2500.h
+  * @author  Jit Kanetkar, Stephen Carter / Based on the LIS3DSH driver by TA Ashraf
+  * @version V1.0.0
+  * @date    11th-November-2015
+  * @brief   Used to communicate with the CC2500 wireless chip. Uses SPI2.
+  ******************************************************************************
+	**/
 #ifndef _INCLUDE_CC2500_H_
 #define _INCLUDE_CC2500_H_
+
+/* includes */
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_spi.h"
+
+/* data structures */
+typedef struct
+{
+	uint8_t tmp;
+} CC2500_InitTypeDef;
+
+/* enums */
+typedef enum {
+	CC2500_PARTNAME = 0x30,
+} CC2500_REG;
+
+typedef enum {
+	CC2500_SPI_CLK = GPIO_Pin_13,
+	CC2500_SPI_MISO = GPIO_Pin_14,
+	CC2500_SPI_MOSI = GPIO_Pin_15,
+	CC2500_SPI_CS = GPIO_Pin_12
+}	CC2500_SPI_Pin;
+
+/* defines */
+#define CC2500_SPI                       SPI2
+#define CC2500_SPI_CLK                   RCC_APB1Periph_SPI2
+
+#define CC2500_SPI_SCK_PIN               GPIO_Pin_13                 /* PA.05 */
+#define CC2500_SPI_SCK_GPIO_PORT         GPIOB                       /* GPIOA */
+#define CC2500_SPI_SCK_GPIO_CLK          RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_SCK_SOURCE            GPIO_PinSource13
+#define CC2500_SPI_SCK_AF                GPIO_AF_SPI2
+
+#define CC2500_SPI_MISO_PIN              GPIO_Pin_14                 /* PA.6 */
+#define CC2500_SPI_MISO_GPIO_PORT        GPIOB                       /* GPIOA */
+#define CC2500_SPI_MISO_GPIO_CLK         RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_MISO_SOURCE           GPIO_PinSource14
+#define CC2500_SPI_MISO_AF               GPIO_AF_SPI2
+
+#define CC2500_SPI_MOSI_PIN              GPIO_Pin_15                 /* PA.7 */
+#define CC2500_SPI_MOSI_GPIO_PORT        GPIOB                       /* GPIOA */
+#define CC2500_SPI_MOSI_GPIO_CLK         RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_MOSI_SOURCE           GPIO_PinSource15
+#define CC2500_SPI_MOSI_AF               GPIO_AF_SPI2
+
+#define CC2500_SPI_CS_PIN                GPIO_Pin_12                 /* PE.03 */
+#define CC2500_SPI_CS_GPIO_PORT          GPIOB                       /* GPIOE */
+#define CC2500_SPI_CS_GPIO_CLK           RCC_AHB1Periph_GPIOB
+#define CC2500_SPI_CS_SOURCE           	 GPIO_PinSource12
+#define CC2500_SPI_CS_AF               	 GPIO_AF_SPI2
+
+//#define CC2500_SPI_INT1_PIN              GPIO_Pin_0                  /* PE.00 */
+//#define CC2500_SPI_INT1_GPIO_PORT        GPIOE                       /* GPIOE */
+//#define CC2500_SPI_INT1_GPIO_CLK         RCC_AHB1Periph_GPIOE
+//#define CC2500_SPI_INT1_EXTI_LINE        EXTI_Line0
+//#define CC2500_SPI_INT1_EXTI_PORT_SOURCE EXTI_PortSourceGPIOE
+//#define CC2500_SPI_INT1_EXTI_PIN_SOURCE  EXTI_PinSource0
+//#define CC2500_SPI_INT1_EXTI_IRQn        EXTI0_IRQn 
+
+//#define CC2500_SPI_INT2_PIN              GPIO_Pin_1                  /* PE.01 */
+//#define CC2500_SPI_INT2_GPIO_PORT        GPIOE                       /* GPIOE */
+//#define CC2500_SPI_INT2_GPIO_CLK         RCC_AHB1Periph_GPIOE
+//#define CC2500_SPI_INT2_EXTI_LINE        EXTI_Line1
+//#define CC2500_SPI_INT2_EXTI_PORT_SOURCE EXTI_PortSourceGPIOE
+//#define CC2500_SPI_INT2_EXTI_PIN_SOURCE  EXTI_PinSource1
+//#define CC2500_SPI_INT2_EXTI_IRQn        EXTI1_IRQn 
+
+/* functions */
+int CC2500_SPI_INIT(CC2500_InitTypeDef init);
 
 #endif // _INCLUDE_CC2500_H_
--- src/cc2500.c
+++ src/cc2500.c
@@ -1,1 +1,69 @@
 #include "cc2500.h"
+
+/* helper functions */
+
+/* source */
+int CC2500_SPI_INIT(CC2500_InitTypeDef init) {
+	GPIO_InitTypeDef gpio_init_s;
+  SPI_InitTypeDef  spi_init_s;
+	
+	/* Enable SPI */
+	//Enable the SPI periph
+  RCC_APB1PeriphClockCmd(CC2500_SPI_CLK, ENABLE);
+
+  // Enable SCK, MOSI and MISO GPIO clocks
+  RCC_AHB1PeriphClockCmd(CC2500_SPI_SCK_GPIO_CLK | CC2500_SPI_MISO_GPIO_CLK | CC2500_SPI_MOSI_GPIO_CLK, ENABLE);
+
+  // Enable CS GPIO clock
+  RCC_AHB1PeriphClockCmd(CC2500_SPI_CS_GPIO_CLK, ENABLE);
+  
+	/* Enable Interrupts over SPI */
+  //// Enable INT1 GPIO clock
+  //RCC_AHB1PeriphClockCmd(CC2500_SPI_INT1_GPIO_CLK, ENABLE);
+  //// Enable INT2 GPIO clock
+  //RCC_AHB1PeriphClockCmd(CC2500_SPI_INT2_GPIO_CLK, ENABLE);
+	
+	/* GPIO Config */
+	GPIO_PinAFConfig(CC2500_SPI_SCK_GPIO_PORT, CC2500_SPI_SCK_SOURCE, CC2500_SPI_SCK_AF);
+  GPIO_PinAFConfig(CC2500_SPI_MISO_GPIO_PORT, CC2500_SPI_MISO_SOURCE, CC2500_SPI_MISO_AF);
+  GPIO_PinAFConfig(CC2500_SPI_MOSI_GPIO_PORT, CC2500_SPI_MOSI_SOURCE, CC2500_SPI_MOSI_AF);
+	GPIO_PinAFConfig(CC2500_SPI_CS_GPIO_PORT, CC2500_SPI_CS_SOURCE, CC2500_SPI_CS_AF);
+
+	gpio_init_s.GPIO_Mode =  GPIO_Mode_AF;
+  gpio_init_s.GPIO_OType = GPIO_OType_PP;
+  gpio_init_s.GPIO_PuPd  = GPIO_PuPd_DOWN;
+  gpio_init_s.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	/* SPI SCK pin configuration */
+  gpio_init_s.GPIO_Pin = CC2500_SPI_SCK_PIN;
+  GPIO_Init(CC2500_SPI_SCK_GPIO_PORT, &gpio_init_s);
+
+  /* SPI  MOSI pin configuration */
+  gpio_init_s.GPIO_Pin =  CC2500_SPI_MOSI_PIN;
+  GPIO_Init(CC2500_SPI_MOSI_GPIO_PORT, &gpio_init_s);
+
+  /* SPI MISO pin configuration */
+  gpio_init_s.GPIO_Pin = CC2500_SPI_MISO_PIN;
+  GPIO_Init(CC2500_SPI_MISO_GPIO_PORT, &gpio_init_s);
+	
+	/* SPI CA pin configuration */
+  gpio_init_s.GPIO_Pin = CC2500_SPI_CS_PIN;
+  GPIO_Init(CC2500_SPI_CS_GPIO_PORT, &gpio_init_s);
+	
+	/* SPI configuration */
+  SPI_I2S_DeInit(CC2500_SPI);
+  spi_init_s.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+  spi_init_s.SPI_DataSize = SPI_DataSize_8b;
+  spi_init_s.SPI_CPOL = SPI_CPOL_Low;
+  spi_init_s.SPI_CPHA = SPI_CPHA_1Edge;
+  spi_init_s.SPI_NSS = SPI_NSS_Soft;
+  spi_init_s.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_32;	// Reduces GPIO clock to 5.25 MHz
+  spi_init_s.SPI_FirstBit = SPI_FirstBit_MSB;
+  spi_init_s.SPI_CRCPolynomial = 7;
+  spi_init_s.SPI_Mode = SPI_Mode_Master;
+  SPI_Init(CC2500_SPI, &spi_init_s);
+	
+	return 0;
+}
+
+
