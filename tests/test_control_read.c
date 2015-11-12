4fb1f7e03bdf12c6b2f1874c21b418b76f0312ac
Jit <kanetkarster@gmail.com>
2015-11-12 14:50:54 -0500

    improved tests

--- tests/test_control_read.c
+++ tests/test_control_read.c
@@ -1,9 +1,22 @@
 #include "all_wireless_tests.h"
 
+const uint8_t PARTNUM = 128, VERSION = 3;
+
 void test_control_read(void) {
 	uint8_t x;
+	uint8_t errct = 0;
 	CC2500_Read(&x, CC2500_PARTNAME, 1);
-	printf("%d\n", x);
+	
+	if (x != PARTNUM) {
+		printf("Error reading PARTNUM!\n"
+						"Read %d instead of %d", x, PARTNUM);
+		errct++;
+	}
+	
 	CC2500_Read(&x, CC2500_VERSION, 1);
-	printf("%d\n", x);
+	if (x != VERSION) {
+		printf("Error reading PARTNUM!\n"
+						"Read %d instead of %d", x, VERSION);
+		errct++;
+	}
 }
