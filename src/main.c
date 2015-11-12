91948beebb0b7092a92e9f767226dc7f16fc2faa
Jit <kanetkarster@gmail.com>
2015-11-12 14:21:36 -0500

    reads status register

--- src/main.c
+++ src/main.c
@@ -14,7 +14,13 @@
  * main: initialize and start the system
  */
 int main (void) {
+	CC2500_SPI_INIT();
 	
+	uint8_t x[4];
+	CC2500_Read(x, CC2500_PARTNAME, 2);
+	printf("%d %d %d %d\n", x[0], x[1], x[2], x[3]);
+	CC2500_Read(x, CC2500_VERSION, 2);
+	printf("%d %d %d %d\n", x[0], x[1], x[2], x[3]);
 }
 
 
